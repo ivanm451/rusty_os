90d8c45a8774a8a210449971b6bbf577bdc41ada
Ivan Morado <melodious@im-silicon-magic-box.mynetworksettings.com>
2023-05-20 21:50:39 -0400

    created config file for bare metal OS, added vga_buffer as part of minimal 64-bit Rust kernel

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,6 +1,6 @@
 // we disable standard libraries to design our OS as close to the metal as possible
 
-#![no_std]
+#![no_std] // don't link the Rust standard library
 #![no_main] // tell compiler not to use normal entry point chain, since we aren't using std lib C
             // that helps us link to crt0 (c runtime event zero) 
 
@@ -12,9 +12,22 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+
+static HELLO: &[u8] = b"Hello World!";
+
 // removed main() {} since we don't use underlying runtime that calls it (crt0)
 // instead we overwrite operating system entry point with our own _start function
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8; // pass string byte in memory 
+
+    // iterate over HELLO string byte and write into vga_buffer, as well as color light cyan (0xb)
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
